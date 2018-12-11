9f2316b4a091505fe84b70ea150900e0916d1db8
Antoine Carof <acarof@kadesh.local>
2018-12-11 17:40:34 +0100

    Reorganize utils.py. Add RDF. Add second label.

--- analyse/run.py
+++ analyse/run.py
@@ -1,5 +1,4 @@
 from utils import *
-import scipy.sparse.csgraph
 import os, sys, time
 
 try:
@@ -21,36 +20,14 @@ data_path = 'data-%s' % name
 if not os.path.isdir(data_path):
     os.mkdir(data_path)
 
-class Molecule(object):
-
-    def __init__(self):
-        self.belongs = []
-
-
 class Carbonates(MDTraj):
 
     def __init__(self, path):
         super(Carbonates, self).__init__(path)
-        self.lbox = 22.23
         self.time_vs_molecule = {}
         self.time_vs_specific_molecules = {}
         self.all_species = []
         self.msd = {}
-
-    def update_label_co2(self):
-        molcut = 1.75
-        label = 'CO2'
-        for index_c in self.types['C']:
-            carbon = self.atom_list[index_c]
-            coord = 0
-            for index_o in self.types['O']:
-                if carbon.distances[index_o] < molcut:
-                    coord += 1
-            if coord == 2:
-                if self.types.get(label) is None:
-                    self.types[label] = [index_c]
-                else:
-                    self.types[label].append(index_c)
 
     def find_connectivity_carbonates(self):
         bound_cut = 1.9
@@ -72,126 +49,24 @@ class Carbonates(MDTraj):
             carbon.was_connected = carbon.connected
             #print carbon.connected
 
-    def find_molecules(self):
-        natom = len(self.atom_list)
-        adjacency = np.zeros( (natom, natom) )
-        for index1, atom in enumerate(self.atom_list):
-            for index2 in atom.connected:
-                adjacency[index1, index2] = 1
-        number_mol, array = scipy.sparse.csgraph.connected_components(adjacency)
-        self.list_molecules = []
-        for i in range(number_mol):
-            self.list_molecules.append( Molecule() )
-        for index, element in enumerate(array.tolist()):
-            self.list_molecules[element].belongs.append(index)
-        #for molecule in self.list_molecules:
-        #    if len(molecule.belongs) == 5:
-        #        print molecule.belongs
-
-    def name_molecules(self):
-        self.types_molecules = {}
-        for molecule in self.list_molecules:
-            raw_label = ''
-            for index_atom in molecule.belongs:
-                raw_label += self.atom_list[index_atom].label
-            molecule.label = ''.join(sorted(raw_label))
-            if self.types_molecules.get(molecule.label) is None:
-                self.types_molecules[molecule.label] = 1
-            else:
-                self.types_molecules[molecule.label] += 1
-            if molecule.label not in self.all_species:
-                self.all_species.append(molecule.label)
-        #print self.all_species
-
-    def calculate_msd(self, list_atoms):
-        for atom in self.atom_list:
-            if atom.label in list_atoms:
-                if self.msd.get(atom.label) is None:
-                    self.msd[atom.label] = {
-                        0.0 : {
-                            'distance' : 0.0,
-                            'counter'  : 1,
-                        }
-                    }
-                for previous in atom.previous_pos:
-                    time = previous[0]
-                    vect = atom.positions - previous[1]
-                    if self.msd[atom.label].get(self.times[-1] - time) is None:
-                        self.msd[atom.label][ self.times[-1] - time] = {
-                            'distance' : 0.0,
-                            'counter'  : 0,
-                        }
-                    self.msd[atom.label][self.times[-1] - time ]['distance'] += np.linalg.norm(vect)**2
-                    self.msd[atom.label][self.times[-1] - time]['counter' ] += 1
-                atom.previous_pos.append( (self.times[-1], atom.positions) )
-        #print self.msd
-
-
-
-
-    def identify_molecule(self, *args):
-        self.specific_molecules = []
-        for molecule in self.list_molecules:
-            if molecule.label in args:
-                self.specific_molecules.append( (molecule.label, molecule.belongs) )
-
     def calculate_properties(self):
-        #start = time.time()
         self.find_distances()
-        #end = time.time()
-        #print "distance", (start -end)
-        #start = time.time()
         self.find_connectivity_carbonates()
-        #end = time.time()
-        #print "connectivity", (start -end)
-        #start = time.time()
         self.find_molecules()
-        #end = time.time()
-        #print "find mol", (start -end)
-        #start = time.time()
         self.name_molecules()
         self.identify_molecule( 'COO', 'CCOOOOO')
-        #end = time.time()
-        #print "name mol", (start -end)
+        self.find_types_mol()
+        self.calculate_rdf('Li', 'K')
         if self.times[-1]%50.0 == 0:
             self.calculate_msd(['C', 'Li', 'K'])
         self.time_vs_molecule[self.times[-1]] = self.types_molecules
         self.time_vs_specific_molecules[self.times[-1]] = self.specific_molecules
 
-
-    def print_specific_molecules(self, *args):
-        with open('%s/specific_molecules.dat' % data_path, 'w') as file_:
-            file_.write('Time Molecule Index\n')
-            for time in sorted(self.time_vs_specific_molecules):
-                if self.time_vs_specific_molecules[time] != []:
-                    for mol in self.time_vs_specific_molecules[time]:
-                        string = '%s  %s  %s ' % (time, mol[0], ' '.join(map(str, mol[1])))
-                        file_.write(string + '\n')
-
-    def print_kind_molecules(self, data_path):
-        with open('%s/kind_molecules.dat' % data_path, 'w') as file_:
-            file_.write('Time %s\n' % '  '.join(self.all_species))
-            for time in sorted(self.time_vs_molecule):
-                string = ' %s ' % time
-                for specy in self.all_species:
-                    string += '  %s   ' % self.time_vs_molecule[time].get(specy, 0)
-                file_.write(string + '\n')
-
-    def print_msd(self, data_path):
-        for atom in self.msd:
-            with open('%s/MSD_%s.dat' % (data_path, atom), 'w') as file_:
-                file_.write('Time  MSD\n')
-                for time in sorted(self.msd[atom]):
-                    msd = self.msd[atom][time]['distance'] / self.msd[atom][time]['counter']
-                    string = '%s  %s\n' % (time, msd)
-                    file_.write(string)
-
-
-
     def print_properties(self, data_path):
         self.print_kind_molecules(data_path)
         self.print_specific_molecules(data_path)
         self.print_msd(data_path)
+        self.print_rdf(data_path)
         #print self.time_vs_molecule
         pass
 
@@ -199,6 +74,7 @@ class Carbonates(MDTraj):
         os.system('cp %s-1.ener %s/' % (self.path, data_path))
 
 traj = Carbonates(traj_path + traj_name)
-traj.analyse_timestep(frequency=frequency) # analyse every 10 steps
+traj.lbox = 22.23
+traj.analyse_timestep(frequency=frequency)
 traj.print_properties(data_path)
 traj.print_energetics(data_path)
--- analyse/utils.py
+++ analyse/utils.py
@@ -1,11 +1,17 @@
 import re
 import numpy as np
 import time
+import scipy.sparse.csgraph
+
+
+class Molecule(object):
+    def __init__(self):
+        self.belongs = []
 
 class atom(object):
-
     def __init__(self, label, positions):
         self.label = label
+        self.label_mol = label
         self.positions = positions
         self.distances = []
         self.connected = []
@@ -17,7 +23,6 @@ class atom(object):
 
 
 class MDTraj(object):
-
     def __init__(self, path):
         self.path = path
         self.rdf = {}
@@ -27,7 +32,6 @@ class MDTraj(object):
         pass
 
     def analyse_timestep(self, frequency = 1):
-
         with open(self.path + '.xyz') as traj:
             self.atom_list = []
             natom = int(traj.readline())
@@ -70,6 +74,16 @@ class MDTraj(object):
                     self.times.append( float(re.findall(pattern, traj.readline())[0]))
         #print self.rdf
 
+    def find_types_mol(self):
+        self.types_mol = {}
+        for index, atom in enumerate(self.atom_list):
+            label = atom.label_mol
+            if self.types_mol.get(label) is None:
+                self.types_mol[label] = [index]
+            else:
+                self.types_mol[label].append(index)
+
+
     def find_distances(self):
         list_index = range(len(self.atom_list))
         new_list = list(list_index)
@@ -85,21 +99,132 @@ class MDTraj(object):
             #print atom1.distances
 
 
+    def find_molecules(self):
+        natom = len(self.atom_list)
+        adjacency = np.zeros( (natom, natom) )
+        for index1, atom in enumerate(self.atom_list):
+            for index2 in atom.connected:
+                adjacency[index1, index2] = 1
+        number_mol, array = scipy.sparse.csgraph.connected_components(adjacency)
+        self.list_molecules = []
+        for i in range(number_mol):
+            self.list_molecules.append( Molecule() )
+        for index, element in enumerate(array.tolist()):
+            self.list_molecules[element].belongs.append(index)
+        #for molecule in self.list_molecules:
+        #    if len(molecule.belongs) == 5:
+        #        print molecule.belongs
+
+    def name_molecules(self):
+        self.types_molecules = {}
+        for molecule in self.list_molecules:
+            raw_label = ''
+            for index_atom in molecule.belongs:
+                raw_label += self.atom_list[index_atom].label
+            molecule.label = ''.join(sorted(raw_label))
+            if self.types_molecules.get(molecule.label) is None:
+                self.types_molecules[molecule.label] = 1
+            else:
+                self.types_molecules[molecule.label] += 1
+            if len(molecule.belongs) > 1:
+                for index_atom in molecule.belongs:
+                    self.atom_list[index_atom].label_mol = '_'.join( (self.atom_list[index_atom].label, molecule.label) )
+            if molecule.label not in self.all_species:
+                self.all_species.append(molecule.label)
+        #print self.all_species
+
+    def identify_molecule(self, *args):
+        self.specific_molecules = []
+        for molecule in self.list_molecules:
+            if molecule.label in args:
+                self.specific_molecules.append( (molecule.label, molecule.belongs) )
+
+
+
+
 
     def calculate_rdf(self, label1, label2):
-        length = 10.0
-        nbin = 1000
-        rdf = np.zeros(nbin)
-        dr = length / nbin
-        for index1 in self.types[label1]:
-            for index2 in self.types[label2]:
+        dr = 0.1
+        nbins = int(self.lbox/(2*dr))
+        rdf = np.zeros(nbins)
+        #dr = length / nbins
+        natom_pairs = 0
+        for index1 in self.types_mol[label1]:
+            for index2 in self.types_mol[label2]:
                 int_ = int( np.rint( self.atom_list[index1].distances[index2] / dr ) )
-                if int_ < nbin:
+                if int_ < nbins:
                     rdf[int_] += 1
+                natom_pairs += 1
         if self.rdf.get((label1, label2)) is None:
-            self.rdf[(label1, label2)] = rdf
+            bins = dr * np.array(range(nbins))
+            self.rdf[(label1, label2)] = [rdf, 1, bins, dr,  natom_pairs]
         else:
-            for index in range(len(self.rdf[(label1, label2)])):
-                self.rdf[(label1, label2)][index] += rdf[index]
+            self.rdf[(label1, label2)][0] += rdf
+            self.rdf[(label1, label2)][1] += 1
+
+    def calculate_msd(self, list_atoms):
+        for atom in self.atom_list:
+            if atom.label in list_atoms:
+                if self.msd.get(atom.label) is None:
+                    self.msd[atom.label] = {
+                        0.0 : {
+                            'distance' : 0.0,
+                            'counter'  : 1,
+                        }
+                    }
+                for previous in atom.previous_pos:
+                    time = previous[0]
+                    vect = atom.positions - previous[1]
+                    if self.msd[atom.label].get(self.times[-1] - time) is None:
+                        self.msd[atom.label][ self.times[-1] - time] = {
+                            'distance' : 0.0,
+                            'counter'  : 0,
+                        }
+                    self.msd[atom.label][self.times[-1] - time ]['distance'] += np.linalg.norm(vect)**2
+                    self.msd[atom.label][self.times[-1] - time]['counter' ] += 1
+                atom.previous_pos.append( (self.times[-1], atom.positions) )
+        #print self.msd
 
 
+
+
+    def print_specific_molecules(self, data_path, *args):
+        with open('%s/specific_molecules.dat' % data_path, 'w') as file_:
+            file_.write('Time Molecule Index\n')
+            for time in sorted(self.time_vs_specific_molecules):
+                if self.time_vs_specific_molecules[time] != []:
+                    for mol in self.time_vs_specific_molecules[time]:
+                        string = '%s  %s  %s ' % (time, mol[0], ' '.join(map(str, mol[1])))
+                        file_.write(string + '\n')
+
+    def print_kind_molecules(self, data_path):
+        with open('%s/kind_molecules.dat' % data_path, 'w') as file_:
+            file_.write('Time %s\n' % '  '.join(self.all_species))
+            for time in sorted(self.time_vs_molecule):
+                string = ' %s ' % time
+                for specy in self.all_species:
+                    string += '  %s   ' % self.time_vs_molecule[time].get(specy, 0)
+                file_.write(string + '\n')
+
+    def print_msd(self, data_path):
+        for atom in self.msd:
+            with open('%s/MSD_%s.dat' % (data_path, atom), 'w') as file_:
+                file_.write('Time  MSD\n')
+                for time in sorted(self.msd[atom]):
+                    msd = self.msd[atom][time]['distance'] / self.msd[atom][time]['counter']
+                    string = '%s  %s\n' % (time, msd)
+                    file_.write(string)
+
+    def print_rdf(self, data_path):
+        for pair in self.rdf:
+            with open('%s/RDF_%s.dat' % (data_path, '_'.join(pair)), 'w') as f:
+                f.write('R   RDF\n')
+                rdf = self.rdf[pair][0]
+                count = self.rdf[pair][1]
+                distance = self.rdf[pair][2]
+                dr = self.rdf[pair][3]
+                natom_pair = self.rdf[pair][4]
+                for (r, val) in zip(distance, rdf):
+                    if r > 0.0:
+                        val = val * self.lbox**3 / (count * 4*np.pi*(r**2) * dr * natom_pair )
+                        f.write('%s   %s\n' % (r, val))
